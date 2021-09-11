69125523912ccd196020463f5d05342de43b3890
Âkif Kılıç <60439616+aakifklcc@users.noreply.github.com>
2021-09-11 23:40:24 +0300

    Add files via upload

---
+++ Algorithm Analysis & Design/src/algorithm/DjExtra/Edge.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deneme2;
+
+/**
+ *
+ * @author akifkilic
+ */
+public class Edge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+          Node n1;
+    Node n2;
+    int cost;
+
+
+    public Edge(Node n1, Node n2, int cost){
+        this.n1 = n1;
+        this.n2 = n2;
+        this.cost = cost;
+
+        //adding n1 as neighbor to n2 and n2 as neighbor to n1
+        if(!n1.neighbors.contains(n2)){
+            n1.add_neighbor(n2);
+        }
+        if(!n2.neighbors.contains(n1)){
+            n2.add_neighbor(n1);
+        }
+
+
+    }
+
+    public int getDistance(){
+        return cost;
+    }
+    public boolean is_proper_edge(String name1, String name2){ //returns whether this edge holds the two nodes passed in or not. This allows us to loop through all the edges to find proper costs
+        boolean result = false;
+        if((name1 == n1.getName() && name2 == n2.getName()) ||( name1 == n2.getName() && name2 == n1.getName())){
+            result = true;
+        }
+        return result;
+    }
+        
+        
+        
+    }
+    
+}
